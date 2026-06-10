aafd0b9bbc766a2f80a408906714effebee26bff
McCarthy0501 <rocr1105@gmail.com>
2026-06-09 22:34:03 -0400

    reparando

--- Proyecto_UniversidadV2_1/BackEnd/proyectoIII/ecommerce_main/accounts/User_serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Account # ¡Importa el modelo desde tu app 'store'!
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Account
-        fields = '__all__'
-
-        
--- Proyecto_UniversidadV2_1/BackEnd/proyectoIII/ecommerce_main/category/serializers.py
+++
@@ -1,9 +0,0 @@
-# api/serializers.py
-
-from rest_framework import serializers
-from .models import Category # ¡Importa el modelo desde tu app 'store'!
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
